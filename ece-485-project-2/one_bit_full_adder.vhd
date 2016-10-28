2137914636d6f4fbcff5319246b9eabe921f7787
Daniel H Meves <dhmeves@gmail.com>
2016-10-27 19:42:52 -0500

    renamed one_bit_full_adder.vhd

--- ece-485-project-2/one_bit_full_adder.vhd
+++ ece-485-project-2/one_bit_full_adder.vhd
@@ -1,14 +1,14 @@
 library IEEE;
 use ieee.std_logic_1164.all;
 
-entity 1_bit_full_adder is
+entity one_bit_full_adder is
 	port(
 		a , b, cin : in std_logic;
-		cout, sum: out std_logic;
+		cout, sum: out std_logic
 	);
-end entity 1_bit_full_adder;
+end entity one_bit_full_adder;
 
-architecture behav of 1_bit_full_adder is
+architecture behav of one_bit_full_adder is
 	begin
 		cout <= (a and b) or (cin and (a xor b));
 		sum <= a xor b xor cin;
