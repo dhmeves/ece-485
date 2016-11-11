6bd4128a5a1cb4ec4e91ce2e2f46cfb30775768c
Daniel H Meves <dhmeves@gmail.com>
2016-11-10 23:00:07 -0600

    fixed alu

--- ece-485-project-2/alu.vhd
+++ ece-485-project-2/alu.vhd
@@ -11,13 +11,18 @@ entity alu is
 end entity alu;
 
 architecture behav of alu is
-	signal amuxoutput, bmuxoutput, andgate, orgate, sum :  std_logic;
+	signal A32, B32, LESS32, amuxoutput, bmuxoutput, andgate, orgate, sum, RESULT32 : std_logic_vector(31 downto 0);
+	
 	begin
+		A32(0) <= a;
+		B32(0) <= b;
+		LESS32(0) <= less;
+		result <= RESULT32(0);
 		--Creating objects of each (Port map sends the inputs for each object and receives output)
 		FULLADDER : entity work.one_bit_full_adder(behav) port map(amuxoutput, bmuxoutput, carryin, carryout, sum);
-		A_MUX : entity work.two_to_one_mux(behav) port map (a, "not"(a), ainvert, amuxoutput);
-		B_MUX : entity work.two_to_one_mux(behav) port map (b, "not"(b), binvert, bmuxoutput);
+		A_MUX : entity work.two_to_one_mux(behav) port map (A32, "not"(A32), ainvert, amuxoutput);
+		B_MUX : entity work.two_to_one_mux(behav) port map (B32, "not"(B32), binvert, bmuxoutput);
 		andgate <= amuxoutput and bmuxoutput;
 		orgate <= amuxoutput or bmuxoutput;
-		OUTMUX : entity work.four_to_one_mux(behav) port map (andgate, orgate, sum, less, ALUOp, result);
+		OUTMUX : entity work.four_to_one_mux(behav) port map (andgate, orgate, sum, LESS32, ALUOp, RESULT32);
 end behav;
--- ece-485-project-2/one_bit_full_adder.vhd
+++ ece-485-project-2/one_bit_full_adder.vhd
@@ -3,13 +3,15 @@ use ieee.std_logic_1164.all;
 
 entity one_bit_full_adder is
 	port(
-		a , b, cin : in std_logic;
-		cout, sum: out std_logic
+		a , b : in std_logic_vector(31 downto 0);
+		cin : in std_logic;
+		cout : out std_logic;
+		sum : out std_logic_vector(31 downto 0)
 	);
 end entity one_bit_full_adder;
 
 architecture behav of one_bit_full_adder is
 	begin
-		cout <= (a and b) or (cin and (a xor b));
-		sum <= a xor b xor cin;
+		cout <= (a(0) and b(0)) or (cin and (a(0) xor b(0)));
+		sum(0) <= a(0) xor b(0) xor cin;
 end behav;
