3c6e7870840e2f794933161089a7543cadc989cf
DoomDawn <fherrer4@hawk.iit.edu>
2016-11-10 22:34:26 -0600

    fixed the 2to1 mux also

--- ece-485-project-2/two_to_one_mux.vhd
+++ ece-485-project-2/two_to_one_mux.vhd
@@ -4,12 +4,14 @@ use ieee.std_logic_1164.all;
 entity two_to_one_mux is
 	port(
 		a, b : in std_logic_vector(31 downto 0); 
-		sel : in std_logic_vector(1 downto 0);
+		sel : in std_logic;
 		output : out std_logic_vector(31 downto 0)
 	);
 end entity two_to_one_mux;
 
 architecture behav of two_to_one_mux is
 	begin
-		output <= (a and (not sel)) or (b and sel);
+		--output <= (a and (not sel)) or (b and sel);
+		output <= a when (sel = '0') else b;
+		
 end behav;
