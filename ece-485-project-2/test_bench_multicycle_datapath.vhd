a52b9a444b2d65d4dbc317e59d052fe1cbe2655a
Daniel H Meves <dhmeves@gmail.com>
2016-11-15 17:59:07 -0600

    test bench written but fails to load design

--- ece-485-project-2/test_bench_multicycle_datapath.vhd
+++ ece-485-project-2/test_bench_multicycle_datapath.vhd
@@ -5,3 +5,35 @@ entity test_bench_multicycle_datapath is
 end test_bench_multicycle_datapath;
 
 architecture behav of test_bench_multicycle_datapath is
+
+component multicycle_datapath is
+	port(
+		clk, rst, pre, ce : in std_logic
+	);
+end component;
+
+signal clk, rst, pre, ce, finished : std_logic := '0';
+signal freq : real;
+signal period : time := 1 sec / freq;
+signal half_period : time := period / 2;
+
+begin
+	test_bench: multicycle_datapath port map(
+		clk => clk,
+		rst => rst,
+		pre => pre,
+		ce => ce
+	);
+
+freq <= 0.01;
+clk <= not clk after half_period when finished /= '1' else '0';
+ce <= '1';
+
+stim_process : process
+begin
+
+	
+	--wait 50 ns;
+
+end process;
+end;
