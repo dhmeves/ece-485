1be6ab7464d61acc1688a7be03338145deb9d8ab
Daniel H Meves <dhmeves@gmail.com>
2016-11-15 23:30:29 -0600

    added 1 bit two input mux

---
+++ ece-485-project-2/two_to_one_mux_1_bit.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use ieee.std_logic_1164.all;
+
+entity two_to_one_mux_5_bit is
+	port(
+		a, b : in std_logic; 
+		sel : in std_logic;
+		output : out std_logic
+	);
+end entity two_to_one_mux_5_bit;
+
+architecture behav of two_to_one_mux_5_bit is
+	begin
+		--output <= (a and (not sel)) or (b and sel);
+		output <= a when (sel = '0') else b;
+		
+end behav;
