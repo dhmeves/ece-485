6ae6189bf2a8be6aac02ba660640938fb03effd8
Daniel H Meves <dhmeves@gmail.com>
2016-11-15 23:31:26 -0600

    for alu

--- ece-485-project-2/two_to_one_mux_1_bit.vhd
+++ ece-485-project-2/two_to_one_mux_1_bit.vhd
@@ -1,15 +1,15 @@
 library IEEE;
 use ieee.std_logic_1164.all;
 
-entity two_to_one_mux_5_bit is
+entity two_to_one_mux_1_bit is
 	port(
 		a, b : in std_logic; 
 		sel : in std_logic;
 		output : out std_logic
 	);
-end entity two_to_one_mux_5_bit;
+end entity two_to_one_mux_1_bit;
 
-architecture behav of two_to_one_mux_5_bit is
+architecture behav of two_to_one_mux_1_bit is
 	begin
 		--output <= (a and (not sel)) or (b and sel);
 		output <= a when (sel = '0') else b;
