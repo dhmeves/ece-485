a6c0649e4711a2a57d1742615d9ed3d79f1d7ea2
DoomDawn <fherrer4@hawk.iit.edu>
2016-11-16 20:51:33 -0600

    dud

--- ece-485-project-2/control_unit.vhd
+++ ece-485-project-2/control_unit.vhd
@@ -11,7 +11,6 @@ entity control_unit is
 end control_unit;
 
 architecture behav of control_unit is
-	--signal states, S_If, S_Id, S_Ex, S_Mem, S_Wb;
 	type states is (S_If, S_Id, S_Ex, S_Mem, S_Wb);
 	signal current_state : states;
 	begin
@@ -35,90 +34,20 @@ architecture behav of control_unit is
 		process(current_state) is
 		begin
 		if (current_state=S_If) then
-			if (op="000000") then			-- add
 				pcWriteCond <= '0';
 				pcWrite <= '0';
 				IorD <= '0';
 				memRead <= '1';
 				memWrite <= '0';
 				memToReg <= '0';
-				irWrite <= '0';
+				irWrite <= '1';
 				ALUSrcA <= '0';
 				regWrite <= '1';
 				regDst <= '1';
 				pcSource <= "00";
-				ALUSrcB <= "00";
-				ALUOp <= "10";
-			elsif (op="100011") then		-- lw
-				pcWriteCond <= '0';
-				pcWrite <= '0';
-				IorD <= '0';
-				memRead <= '1';
-				memWrite <= '0';
-				memToReg <= '1';
-				irWrite <= '0';
-				ALUSrcA <= '1';
-				regWrite <= '1';
-				regDst <= '0';
-				pcSource <= "00";
-				ALUSrcB <= "00";
+				ALUSrcB <= "01";
 				ALUOp <= "00";
-			elsif (op="101011") then		--sw
-				pcWriteCond <= '0';
-				pcWrite <= '0';
-				IorD <= '0';
-				memRead <= '1';
-				memWrite <= '1';
-				memToReg <= 'X';
-				irWrite <= '0';
-				ALUSrcA <= '1';
-				regWrite <= '0';
-				regDst <= 'X';
-				pcSource <= "00";
-				ALUSrcB <= "00";
-				ALUOp <= "00";
-			elsif (op="000100") then		-- bne/beq
-				pcWriteCond <= '0';
-				pcWrite <= '0';
-				IorD <= '0';
-				memRead <= '0';
-				memWrite <= '0';
-				memToReg <= 'X';
-				irWrite <= '0';
-				ALUSrcA <= '0';
-				regWrite <= '0';
-				regDst <= 'X';
-				pcSource <= "00";
-				ALUSrcB <= "00";
-				ALUOp <= "01";
-			elsif (op="010000") then		-- or
-				pcWriteCond <= '0';
-				pcWrite <= '0';
-				IorD <= '0';
-				memRead <= '0';
-				memWrite <= '0';
-				memToReg <= '0';
-				irWrite <= '0';
-				ALUSrcA <= '0';
-				regWrite <= '0';
-				regDst <= '0';
-				pcSource <= "00";
-				ALUSrcB <= "00";
-				ALUOp <= "00";
-			elsif (op="010001") then		-- andi
-				pcWriteCond <= '0';
-				pcWrite <= '0';
-				IorD <= '0';
-				memRead <= '0';
-				memWrite <= '0';
-				memToReg <= '0';
-				irWrite <= '0';
-				ALUSrcA <= '0';
-				regWrite <= '0';
-				regDst <= '0';
-				pcSource <= "00";
-				ALUSrcB <= "00";
-				ALUOp <= "00";
+			
 			end if;
 		end if;
 
